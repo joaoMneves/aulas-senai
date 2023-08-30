e3e1e981c9346c14fd6326ba723ebba82e87ece4
João Miguel Neves <joaomiguelneves2007@proton.me>
2023-08-30 10:26:40 -0300

    style: listas 2, 3 e 4

--- src/lista2.java
+++ src/lista2.java
@@ -26,41 +26,44 @@ public class lista2 {
         input.close();
 
     }
-//    1- Faça um programa para ler um número inteiro, e e depois dizer
-//se este número é negativo ou não.
-    static void ex1(Scanner input){
+
+    // 1- Faça um programa para ler um número inteiro, e e depois dizer
+    // se este número é negativo ou não.
+    static void ex1(Scanner input) {
         System.out.print("digite um numero ");
         int num = input.nextInt();
-        if (num > 0){
+        if (num > 0) {
             System.out.println("positivo");
         } else {
             System.out.println("negativo");
         }
     }
-//    Faça um programa para ler um número inteiro e dizer se este
-//número é par ou impar.
-    static void ex2(Scanner input){
+
+    // Faça um programa para ler um número inteiro e dizer se este
+    // número é par ou impar.
+    static void ex2(Scanner input) {
         System.out.print("digite um numero ");
         int num = input.nextInt();
-        if (num % 2 == 0){
+        if (num % 2 == 0) {
             System.out.println("par");
         } else {
             System.out.println("impar");
         }
     }
-//    Leia 2 valores inteiros (A e B). Após, o programa deve mostrar O
-//uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indi-
-//cando se os valores lidos são múltiplos entre si. Atenção: os núme-
-//ros devem poder ser digitados em ordem crescente ou decres-
-//cente.
-    static void ex3(Scanner input){
+
+    // Leia 2 valores inteiros (A e B). Após, o programa deve mostrar O
+    // uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indi-
+    // cando se os valores lidos são múltiplos entre si. Atenção: os núme-
+    // ros devem poder ser digitados em ordem crescente ou decres-
+    // cente.
+    static void ex3(Scanner input) {
         System.out.print("digite um numero ");
         int num = input.nextInt();
 
         System.out.print("digite um numero ");
         int num2 = input.nextInt();
 
-        if (num % num2 == 0){
+        if (num % num2 == 0) {
             System.out.println("São multiplos");
         } else if (num2 % num == 0) {
             System.out.println("São multiplos");
@@ -68,10 +71,11 @@ public class lista2 {
             System.out.println("Nao sao Multiplos");
         }
     }
-//    Leia hora inicial e a hora final de um jogo. A seguir calcule a
-//duração do Jogo, sabendo que O mesmo pode começar em um dia
-//e terminar em outro, tendo uma duração mínima de 1 hora e má-
-//xima de 24 horas
+
+    // Leia hora inicial e a hora final de um jogo. A seguir calcule a
+    // duração do Jogo, sabendo que O mesmo pode começar em um dia
+    // e terminar em outro, tendo uma duração mínima de 1 hora e má-
+    // xima de 24 horas
     static void ex4(Scanner input) {
         System.out.print("digite a hora inicial ");
         int num = input.nextInt();
@@ -86,16 +90,17 @@ public class lista2 {
         }
 
     }
-//    Com base na tabela abaixo, escreva um programa que leia o có-
-//digo de um item e a quantidade deste item. A seguir, calcule e
-//mostre o valor da conta a pagar:
-//    Código          Descrição  Preço
-//    1               Pizza       4
-//    2               Xburger      4,5
-//    3               Xbacon        5
-//    4               Xsalada       2
-//    5               Refri       1,5
-    static void ex5(Scanner input){
+
+    // Com base na tabela abaixo, escreva um programa que leia o có-
+    // digo de um item e a quantidade deste item. A seguir, calcule e
+    // mostre o valor da conta a pagar:
+    // Código Descrição Preço
+    // 1 Pizza 4
+    // 2 Xburger 4,5
+    // 3 Xbacon 5
+    // 4 Xsalada 2
+    // 5 Refri 1,5
+    static void ex5(Scanner input) {
         System.out.print("\t1\tPizza" +
                 "\n\t2\tXburger" +
                 "\n\t3\tXbacon" +
@@ -115,11 +120,12 @@ public class lista2 {
             default -> System.out.println("digite um codigo valido");
         }
     }
-//    Faça um programa que leia um valor qualquer e apresente uma
-//mensagem dizendo em qual dos seguintes intervalos ([0,25],
-//(25,50], (50,75], (75,100]) este valor se encontra.Se o valor não
-//estiver em nenhum destes intervalos, deverá ser impressa a men-
-//sagem "Fora de intervalo"
+
+    // Faça um programa que leia um valor qualquer e apresente uma
+    // mensagem dizendo em qual dos seguintes intervalos ([0,25],
+    // (25,50], (50,75], (75,100]) este valor se encontra.Se o valor não
+    // estiver em nenhum destes intervalos, deverá ser impressa a men-
+    // sagem "Fora de intervalo"
     static void ex6(Scanner input) {
         System.out.print("digite um valor: ");
         int num = input.nextInt();
@@ -136,11 +142,12 @@ public class lista2 {
             System.out.println("não esta em nenhum intervalo");
         }
     }
-//    Leia 2 valores com uma casa decimal (x e y), que devem repre-
-//sentar as coordenadas de um ponto em um plano. A seguir, determine
-// qual o quadrante ao qual pertence o ponto, ou se se está sobre
-//um dos eïxos cartesianos ou na origem (x= y=0).
-    static void ex7(Scanner input){
+
+    // Leia 2 valores com uma casa decimal (x e y), que devem repre-
+    // sentar as coordenadas de um ponto em um plano. A seguir, determine
+    // qual o quadrante ao qual pertence o ponto, ou se se está sobre
+    // um dos eïxos cartesianos ou na origem (x= y=0).
+    static void ex7(Scanner input) {
         System.out.print("digite o valor X: ");
         float x = input.nextFloat();
 
@@ -159,16 +166,17 @@ public class lista2 {
             System.out.println("4 quadrante");
         }
     }
-//    8- Faça um programa que leia um valor com duas casas de-
-//cimais, equivalente ao salário de uma pessoa. Em seguida
-//calcule e mostre o valor que esta þessoa deve pagar de Imposto de Renda,
-// segundo a tabela abaixo
-//    Base calculo                      Aliquota
-//    de R$ 1903,99 até R$ 2826,65          7,5%
-//    de R$ 2826,65 até R$ 3751,05          15,0%
-//    de R$ 3751,06 até R$ 4664,68          22,5%
-//    acima de R$ 4664,68                   27,5%
-    static void ex8(Scanner input){
+
+    // 8- Faça um programa que leia um valor com duas casas de-
+    // cimais, equivalente ao salário de uma pessoa. Em seguida
+    // calcule e mostre o valor que esta þessoa deve pagar de Imposto de Renda,
+    // segundo a tabela abaixo
+    // Base calculo Aliquota
+    // de R$ 1903,99 até R$ 2826,65 7,5%
+    // de R$ 2826,65 até R$ 3751,05 15,0%
+    // de R$ 3751,06 até R$ 4664,68 22,5%
+    // acima de R$ 4664,68 27,5%
+    static void ex8(Scanner input) {
         System.out.print("Qual o salario: ");
         double salario = input.nextDouble();
 
--- src/lista3.java
+++ src/lista3.java
@@ -22,24 +22,31 @@ public class lista3 {
         input.close();
 
     }
-//  1 - Faça um programa que repita a leitura de uma senha até que ela seja válida.
-//  Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida".
-//  Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. 
-// Considere que a senha correta é o valor 2002.
-    static void ex1(Scanner input){
+
+    // 1 - Faça um programa que repita a leitura de uma senha até que ela seja
+    // válida.
+    // Para cada leitura de senha incorreta informada, escrever a mensagem "Senha
+    // Invalida".
+    // Quando a senha for informada corretamente deve ser impressa a mensagem
+    // "Acesso Permitido" e o algoritmo encerrado.
+    // Considere que a senha correta é o valor 2002.
+    static void ex1(Scanner input) {
         int senha = 0;
-        while(senha != 2002){
+        while (senha != 2002) {
             System.out.print("\nsenha incorreta");
             System.out.print("\ncoloque a senha: ");
             senha = input.nextInt();
         }
         System.out.print("\nsenha correta");
     }
-// 2-  Faça um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano. 
-// Para cada ponto escrever o quadrante a que ele pertence. 
-// O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
-    static void ex2(Scanner input){
-        while(true){
+
+    // 2- Faça um programa para ler as coordenadas (X,Y) de uma quantidade
+    // indeterminada de pontos no sistema cartesiano.
+    // Para cada ponto escrever o quadrante a que ele pertence.
+    // O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA
+    // (nesta situação sem escrever mensagem alguma).
+    static void ex2(Scanner input) {
+        while (true) {
             System.out.print("digite o valor X: ");
             float x = input.nextFloat();
 
@@ -60,27 +67,32 @@ public class lista3 {
             }
         }
     }
-// 3-  Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. 
-// Escreva um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma:
-//  1.Álcool 2.Gasolina 3.Diesel 4.Fim). 
-// Caso o usuário informe um código inválido (fora da faixa de 1 a 4)
-//  deve ser solicitado um novo código (até que seja válido). 
-// O programa será encerrado quando o código informado for o número 4.
-//  Deve ser escrito a mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme exemplo.
-        static void ex3(Scanner input){
-            int alcool = 0;
-            int gasolina = 0;
-            int diesel = 0; 
-            int cod = 0;
-            while(cod != 4) {
-                System.out.print("combustivel abastecido: \n\t1.Alcool \n\t2.Gasolina \n\t3.Diesel \n\t4.Fim \n\t digite: ");
-                cod = input.nextInt();
-                switch(cod) {
-                    case 1 -> alcool ++;
-                    case 2 -> gasolina++;
-                    case 3 -> diesel++;
-                }
+
+    // 3- Um Posto de combustíveis deseja determinar qual de seus produtos tem a
+    // preferência de seus clientes.
+    // Escreva um algoritmo para ler o tipo de combustível abastecido (codificado da
+    // seguinte forma:
+    // 1.Álcool 2.Gasolina 3.Diesel 4.Fim).
+    // Caso o usuário informe um código inválido (fora da faixa de 1 a 4)
+    // deve ser solicitado um novo código (até que seja válido).
+    // O programa será encerrado quando o código informado for o número 4.
+    // Deve ser escrito a mensagem: "MUITO OBRIGADO" e a quantidade de clientes que
+    // abasteceram cada tipo de combustível, conforme exemplo.
+    static void ex3(Scanner input) {
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+        int cod = 0;
+        while (cod != 4) {
+            System.out
+                    .print("combustivel abastecido: \n\t1.Alcool \n\t2.Gasolina \n\t3.Diesel \n\t4.Fim \n\t digite: ");
+            cod = input.nextInt();
+            switch (cod) {
+                case 1 -> alcool++;
+                case 2 -> gasolina++;
+                case 3 -> diesel++;
             }
-            System.out.printf("\n\tAlcool: %s  \n\tGasolina: %s \n\tDiesel: %s", alcool, gasolina, diesel);
         }
+        System.out.printf("\n\tAlcool: %s  \n\tGasolina: %s \n\tDiesel: %s", alcool, gasolina, diesel);
+    }
 }
